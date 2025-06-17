68da46ad34fd43664ee918973276e702d4ecb718
Md Zalish Mahmud <zalish.shawon@gmail.com>
2025-06-17 10:03:13 +0600

    Implement user management routes: add create, read, update, and delete functionality

--- dist/app/controllers/user.controller.js
+++ dist/app/controllers/user.controller.js
@@ -1,1 +1,63 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.usersRoutes = void 0;
+const express_1 = __importDefault(require("express"));
+const user_model_1 = require("../models/user.model");
+exports.usersRoutes = express_1.default.Router();
+exports.usersRoutes.post("/create-note", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const body = req.body;
+    const user = yield user_model_1.User.create(body);
+    res.status(201).json({
+        success: true,
+        message: "User created successfully",
+        user,
+    });
+}));
+exports.usersRoutes.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const user = yield user_model_1.User.find();
+    res.status(201).json({
+        success: true,
+        message: "users read successfully",
+        user
+    });
+}));
+exports.usersRoutes.get("/:userId", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const userId = req.params.userId;
+    const user = yield user_model_1.User.findById(userId);
+    res.status(201).json({
+        success: true,
+        message: "users read successfully",
+        user
+    });
+}));
+exports.usersRoutes.patch("/:userId", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const userId = req.params.userId;
+    const updatedBody = req.body;
+    const user = yield user_model_1.User.findByIdAndUpdate(userId, updatedBody);
+    res.status(201).json({
+        success: true,
+        message: "User updated successfully",
+        user
+    });
+}));
+exports.usersRoutes.delete("/:userId", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const userId = req.params.userId;
+    const user = yield user_model_1.User.findByIdAndDelete(userId);
+    res.status(201).json({
+        success: true,
+        message: "User deleted successfully",
+        user
+    });
+}));
---
+++ src/app/controllers/user.controller.ts
@@ -0,0 +1,60 @@
+import express, { Request, Response } from "express";
+import { User } from "../models/user.model";
+
+export const usersRoutes = express.Router();
+
+usersRoutes.post("/create-note", async (req: Request, res: Response) => {
+
+    const body = req.body;
+    
+    const user = await User.create(body);
+    res.status(201).json({
+        success: true,
+        message: "User created successfully",
+       user,
+    })
+});
+
+usersRoutes.get("/", async (req: Request, res: Response) => {
+
+    const user = await User.find();
+    res.status(201).json({
+        success: true,
+        message: "users read successfully",
+       user
+    })
+});
+
+usersRoutes.get("/:userId", async (req: Request, res: Response) => {
+
+    const userId = req.params.userId;
+    const user = await User.findById(userId);
+    res.status(201).json({
+        success: true,
+        message: "users read successfully",
+       user
+    })
+});
+
+usersRoutes.patch("/:userId", async (req: Request, res: Response) => {
+
+    const userId = req.params.userId;
+    const updatedBody = req.body;
+    const user = await User.findByIdAndUpdate(userId, updatedBody)
+    res.status(201).json({
+        success: true,
+        message: "User updated successfully",
+       user
+    })
+});
+
+usersRoutes.delete("/:userId", async (req: Request, res: Response) => {
+
+    const userId = req.params.userId;
+    const user = await User.findByIdAndDelete(userId)
+    res.status(201).json({
+        success: true,
+        message: "User deleted successfully",
+       user
+    })
+});
